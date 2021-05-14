817feec27b5b58d2c32e5d11c6e5c68f8bb0f42f
PolinaPar2003 <paramonowa.polina2003.ru@yandex.ru>
2021-05-14 19:52:04 +0300

    first commit

--- project/main.py
+++ project/main.py
@@ -8,6 +8,7 @@ from data.animals import Animals
 from data.souvenirs import Souvenirs
 from data import db_session
 from forms.tickets import TicketsForm
+import os
 from forms.register import RegisterForm
 from forms.reviews import ReviewsForm
 from forms.login import LoginForm
@@ -230,7 +231,8 @@ def register():
 
 def main():
     db_session.global_init("db/oceanarium.db")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
